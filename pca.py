c43e059e56cbf6bc77c4101bd65e64876579673b
Sebastian Correa <scorrea92@hotmail.com>
2018-02-12 21:16:08 +0100

    algo mas ordenado guardando transformacion de pca, falta funcion que devuelva test listo para modelo

--- pca.py
+++ pca.py
@@ -3,17 +3,35 @@ import numpy as np
 from numpy import genfromtxt
 from sklearn.preprocessing import StandardScaler
 from sklearn import decomposition
+from sklearn.externals import joblib
 
-imp_consum = genfromtxt('../dataset_cajamar/imp_consum.csv', delimiter=',')
+def getData_standard(path):
+    imp_consum = genfromtxt(path, delimiter=',')
+    x_std = StandardScaler().fit_transform(imp_consum)
+    a = x_std
+    return a
 
-x_std = StandardScaler().fit_transform(imp_consum)
-# cov = np.cov(x_std.T)
-# ev , eig = np.linalg.eig(cov)
-# a = eig.dot(x_std.T)
+def get_total_pca(path, n_impcons, n_impsald, n_numope, pca_value, recall):
+    impcons = getData_standard(path + n_impcons)
+    impsald = getData_standard(path + n_impsald)
+    numope = getData_standard(path + n_numope) 
+    total = np.column_stack((impcons, impsald, numope))
+    print(total.shape)
+    
+    if recall:
+        pca = decomposition.PCA(n_components=pca_value, svd_solver='full')
+        pca.fit(total)
+        total_pca = pca.transform(total)
+        joblib.dump(pca, 'pca_impcons_impsald_numope.pkl')
+    else:
+        pca = joblib.load('pca_impcons_impsald_numope.pkl')
+        total_pca = pca.transform(total) 
 
-pca = decomposition.PCA(n_components=0.95, svd_solver='full')
-pca.fit(x_std)
-a = pca.transform(x_std)
+    return total_pca
+
+# def test_to_pca(test):
+
+a = get_total_pca('../dataset_cajamar/', 'impcons.csv', 'impsald.csv', 'numope.csv', 0.95, True)
 
 print(a.shape)
 print(a)

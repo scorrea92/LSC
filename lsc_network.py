49b3a67074ee7625903f52ebb4b7d03886ce27f7
Cristobal Barrientos <cbarrientoslow@gmail.com>
2018-02-15 13:08:28 +0100

    Update network

--- lsc_network.py
+++ lsc_network.py
@@ -1,55 +1,31 @@
-from __future__ import print_function
+import keras
+import numpy as np
 
-import numpy
-import pandas
 from keras.models import Sequential
 from keras.layers import Dense
-from keras.wrappers.scikit_learn import KerasRegressor
-from keras.callbacks import TensorBoard
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import KFold
-from sklearn.preprocessing import StandardScaler
-from sklearn.pipeline import Pipeline
 
-# define base model
-def baseline_model():
-    # create model
-    model = Sequential()
-    model.add(Dense(13, input_dim=13, kernel_initializer='normal', activation='relu'))
-    model.add(Dense(1, kernel_initializer='normal'))
-    # Compile model
-    model.compile(loss='mean_squared_error', optimizer='adam')
-    return model
+# Get train and tes
 
-
-# define the model
-def larger_model():
-    # create model
-    model = Sequential()
-    model.add(Dense(13, input_dim=13, kernel_initializer='normal', activation='relu'))
-    model.add(Dense(6, kernel_initializer='normal', activation='relu'))
-    model.add(Dense(1, kernel_initializer='normal'))
-    # Compile model
-    model.compile(loss='mean_squared_error', optimizer='adam')
-    return model
-
-#load dataset
-dataframe = pandas.read_csv("housing.csv",delim_whitespace=True, header=None)
-dataset = dataframe.values
-
-#split into input (X) and output (Y) variables
-X = dataset[:,0:13]
-Y = dataset[:,13]
-
-# evaluate model with standardized dataset
+epochs = 100
+batchsize = 128
 seed = 7
-numpy.random.seed(seed)
-estimators = []
-estimators.append(('standardize', StandardScaler()))
-estimators.append(('mlp', KerasRegressor(build_fn=larger_model, epochs=50, batch_size=5, verbose=0)))
-pipeline = Pipeline(estimators)
-kfold = KFold(n_splits=10, random_state=seed)
-results = cross_val_score(pipeline, X, Y, cv=kfold)
-print("Standardized: %.2f (%.2f) MSE" % (results.mean(), results.std()))
+np.random.seed(seed)
+
+def basic_model():
+    model = Sequential()
+    model.add(Dense(2048, input_shape=(11,), init='uniform', activation='relu'))
+    model.add(Dense(1024, init='uniform', activation='relu'))
+    model.add(Dense(512, init='uniform', activation='relu'))
+    model.add(Dense(1, init='uniform', activation='linear'))
+    model.summary()
+    model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
+    return model
+
+# Tensorboard
+tbCallBack = keras.callbacks.TensorBoard(log_dir='/tmp/keras_logs', write_graph=True)
+
+# Compile and fit
+model = basic_model()
+model.fit(x_train, y_train, epochs=epochs, batch_size=batchsize,  verbose=1, validation_data={x_val,y_val}, callbacks=[tbCallBack])
 
 
